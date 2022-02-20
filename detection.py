ad293411a722e660ccf401a97c3355e5bb0d5eae
Sri-Sai-Charan <svellise@umd.edu>
2022-02-20 17:08:54 -0500

    Found contours using in-built functions

--- detection.py
+++ detection.py
@@ -5,7 +5,8 @@ from scipy.fftpack import fft, fft2, ifft
 # from scipy import fft, fftfreq, fftshift
 import cv2 as cv
 import matplotlib.pyplot as plt
-
+cv.namedWindow('frame',cv.WINDOW_NORMAL)
+cv.resizeWindow('frame', (600, 600))
 
 # def find_edges(frame):
 #     rows, cols = frame.shape
@@ -78,16 +79,47 @@ def fft_func(frame):
     # ax3.title.set_text('FFT + Mask')
     # ax4 = fig.add_subplot(2,2,4)
     # fig.imshow(img_back, cmap='gray')
-    plt.imshow(img_back)
+    # plt.imshow(img_back)
     # ax4.title.set_text('After inverse FFT')
-     
-    plt.show()
+    
+    # plt.show()
+    # print(img_back.shape)
+    # cv.imshow('frame',img_back)
+    return img_back
 
+def corner_detection(frame):
+    # gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+    corners = cv.goodFeaturesToTrack(frame,25,0.01,10)
+    corners = np.int0(corners)
+    for i in corners:
+        x,y = i.ravel()
+        cv.circle(frame,(x,y),3,255,-1)
+    plt.imshow(frame),plt.show()
+
+def contour_mapping(frame):
+    img_blur = cv.GaussianBlur(frame,(3,3), 0)
+    edges = cv.Canny(image=img_blur, threshold1=100, threshold2=200)
+    img = edges 
+    contours, hierarchy = cv.findContours(img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    i = 0
+    out = np.zeros_like(img)
+    for c in contours:
+        if (30000 < int(cv.contourArea(c)) < 40000):           
+            mask = np.zeros_like(img)
+            cv.drawContours(mask,contours,i,(255),cv.FILLED) 
+            # img_pl = np.zeros_like(img)/
+            out[mask == 255] = img[mask == 255]
+            # corner_detection(out)
+            cv.imshow('frame',out)
+            cv.waitKey(0)
+        i+=1
+
+# def    
     
 def gray_conversion(frame):
     gray_img = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
-    # _,binary_img = cv.threshold(gray_img, 127, 255, cv.THRESH_BINARY)
     return gray_img
+
 
 def main():
     cap = cv.VideoCapture('1tagvideo.mp4')
@@ -99,11 +131,10 @@ def main():
         
         if ret == False:
             break
-        # cv.imshow('Frame',frame)
-        gray = gray_conversion(frame)
-        fft_func(gray)
-        # cv.imshow("Gray",gray)
-        # find_edges(gray)
+        
+        
+        contour_mapping(frame)
+        
         break
     cv.waitKey(0)
     cap.release()
